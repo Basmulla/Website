1ea33174b39826d17ce363b3085de0177e41c74e
Basmulla <basmulla1087@gmail.com>
2022-04-06 13:36:38 -0400

    new change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SWK from './pages/SWK';
 import Header from './components/Header'
 import Footer from './components/Footer';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import './App.css';
 
@@ -15,14 +15,14 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/Home" component={Home} />
-          <Route path="/About" component={About} />
-          <Route path="/SWK" component={SWK} />
-        </Switch>
-        <Footer />
-      </Router>
-    </div>
+        <Route exact path="/Website"component={Home}/>
+        <Route exact path="/Home" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/SWK" component={SWK} />
+      </Switch>
+      <Footer />
+    </Router>
+    </div >
   );
 }
 
